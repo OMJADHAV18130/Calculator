a08d5daf577818859010c1c2c118e0405768b172
OMJADHAV18130 <omvjadhav18130@gmail.com>
2025-09-14 17:56:32 +0530

    add main.py

---
+++ main.py
@@ -0,0 +1,90 @@
+# Imports 
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout
+from PyQt5.QtGui import QFont
+
+# App Settings
+
+class CalcApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Calculator App")
+        self.resize(300,400)
+
+        # All Objects
+        self.text_box = QLineEdit()
+        self.grid = QGridLayout()
+        self.text_box.setStyleSheet("QLineEdit {background-color : yellow; color : black; font-size : 24; margin : 10px; padding : 10px;font-style : bold}")
+
+        self.buttons = ["7","8","9","/",
+                        "4","5","6","+",
+                        "1","2","3","-",
+                        "0",".","=","*"]
+
+        row = 0
+        col = 0
+
+        for text in self.buttons:
+            button = QPushButton(text)
+            button.clicked.connect(self.button_click)
+            button.setStyleSheet("QPushButton{font : 25px Comic Sans MS; padding : 10px}")
+            self.grid.addWidget(button, row, col)
+            col += 1
+            
+            if col > 3:
+                col = 0
+                row += 1
+
+
+        self.clear = QPushButton("Clear")
+        self.delete = QPushButton("Delete")
+        self.clear.setStyleSheet("QPushButton{font : 25pt Comic Sans MS; padding : 10px;}")
+        self.delete.setStyleSheet("QPushButton{font : 25pt Comic Sans MS; padding : 10px;}")
+        
+        master_layout = QVBoxLayout()
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
+
+        button_row = QHBoxLayout()
+        button_row.addWidget(self.clear)
+        button_row.addWidget(self.delete)
+
+        master_layout.addLayout(button_row)
+        master_layout.setContentsMargins(25,25,25,25)
+
+        self.setLayout(master_layout)
+
+        self.clear.clicked.connect(self.button_click)
+        self.delete.clicked.connect(self.button_click)
+
+    def button_click(self):
+        button = app.sender()
+        text = button.text()
+
+        if text == "=":
+            symbol = self.text_box.text()
+            try:
+                res = eval(symbol)
+                self.text_box.setText(str(res))
+
+            except Exception as e:
+                print("Error:",e)
+
+        elif text == "Clear":
+            self.text_box.clear()
+
+        elif text == "Delete":
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value[:-1])
+
+        else:
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value + text) 
+
+# Show Run
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main_window = CalcApp()
+    main_window.setStyleSheet("QWidget {background-color: crimson}")
+    main_window.show()
+    app.exec_()
